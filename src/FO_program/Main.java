af16232eacf1887f6884997bde76bc4ced78b70f
f-ousley <++GardenWin++9>
2022-11-10 13:02:27 -0700

    Third Commit

--- src/FO_program/Main.java
+++ src/FO_program/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         primaryStage.setTitle("Login");
-        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.setScene(new Scene(root, 250, 300));
         primaryStage.show();
         JDBC.openConnection();
     }
